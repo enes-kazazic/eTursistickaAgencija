4c00cec677a8fd2d00e350d4d759b7b723a318e5
Enes Kazazic <enes.kazazic@edu.fit.ba>
2021-01-21 18:02:44 +0100

    Add project files.

---
+++ TuristickaAgencija/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TuristickaAgencija.EntityModels;
+
+namespace TuristickaAgencija
+{
+	public class Context:DbContext
+	{
+		public DbSet<Smjestaj> Smjestaji { get; set; }
+
+		public Context(DbContextOptions<Context> options) : base(options)
+		{
+		}
+	}
+}
