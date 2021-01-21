31fe234d01217ac7812c6934912028bfad658886
Enes Kazazic <enes.kazazic@edu.fit.ba>
2021-01-21 18:14:16 +0100

    added migrations

---
+++ TuristickaAgencija/Migrations/20210108210733_incijalna.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TuristickaAgencija.Migrations
+{
+    public partial class incijalna: Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Smjestaji",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(nullable: true),
+                    BrojUgovora = table.Column<string>(nullable: true),
+                    ProvizijaPoOsobi = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Smjestaji", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Smjestaji");
+        }
+    }
+}
