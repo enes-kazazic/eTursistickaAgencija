9f283c91f4a9b4651e22d522727a186bef9c2e8d
DESKTOP-1N7K6JO\User <amel030201@gmail.com>
2021-02-19 16:41:24 +0100

    Dodane 3 tabele u context

--- TuristickaAgencija/Context.cs
+++ TuristickaAgencija/Context.cs
@@ -12,8 +12,12 @@ namespace TuristickaAgencija
 	{
 		public DbSet<Smjestaj> Smjestaji { get; set; }
 		public DbSet<Destinacija> Destinacije { get; set; }
+        public DbSet<Placanje> Placanje { get; set; }
+        public DbSet<Rezervacija> Rezervacija { get; set; }
+        public DbSet<OdabirSmjestaja> OdabirSmjestaja { get; set; }
 
-		public Context(DbContextOptions<Context> options) : base(options)
+
+        public Context(DbContextOptions<Context> options) : base(options)
 		{
 		}
 	}
