438239ea12397b7e8f68a9262bf52c0d8e4cf7a5
DESKTOP-1N7K6JO\User <amel030201@gmail.com>
2021-02-19 16:43:09 +0100

    Migracija za nove tabele OdabirSmjestaja, Plaćanje i Rezervacija

---
+++ TuristickaAgencija/Migrations/20210219153000_TuristickaAgencija5Tabela.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TuristickaAgencija.Migrations
+{
+    public partial class TuristickaAgencija5Tabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rezervacija",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Datum = table.Column<DateTime>(nullable: false),
+                    Cijena = table.Column<double>(nullable: false),
+                    BrojRacuna = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rezervacija", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Smjestaji",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(nullable: true),
+                    BrojUgovora = table.Column<string>(nullable: true),
+                    ProvizijaPoOsobi = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Smjestaji", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Placanje",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Iznos = table.Column<double>(nullable: false),
+                    Datum = table.Column<DateTime>(nullable: false),
+                    NacinPlacanja = table.Column<string>(nullable: true),
+                    RezervacijaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Placanje", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Placanje_Rezervacija_RezervacijaId",
+                        column: x => x.RezervacijaId,
+                        principalTable: "Rezervacija",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Destinacije",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Grad = table.Column<string>(nullable: true),
+                    Drzava = table.Column<string>(nullable: true),
+                    SmjestajId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destinacije", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Destinacije_Smjestaji_SmjestajId",
+                        column: x => x.SmjestajId,
+                        principalTable: "Smjestaji",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OdabirSmjestaja",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VrstaSobe = table.Column<string>(nullable: true),
+                    BrojSobe = table.Column<int>(nullable: false),
+                    SmjestajId = table.Column<int>(nullable: true),
+                    RezervacijaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OdabirSmjestaja", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OdabirSmjestaja_Rezervacija_RezervacijaId",
+                        column: x => x.RezervacijaId,
+                        principalTable: "Rezervacija",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_OdabirSmjestaja_Smjestaji_SmjestajId",
+                        column: x => x.SmjestajId,
+                        principalTable: "Smjestaji",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Destinacije_SmjestajId",
+                table: "Destinacije",
+                column: "SmjestajId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OdabirSmjestaja_RezervacijaId",
+                table: "OdabirSmjestaja",
+                column: "RezervacijaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OdabirSmjestaja_SmjestajId",
+                table: "OdabirSmjestaja",
+                column: "SmjestajId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Placanje_RezervacijaId",
+                table: "Placanje",
+                column: "RezervacijaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destinacije");
+
+            migrationBuilder.DropTable(
+                name: "OdabirSmjestaja");
+
+            migrationBuilder.DropTable(
+                name: "Placanje");
+
+            migrationBuilder.DropTable(
+                name: "Smjestaji");
+
+            migrationBuilder.DropTable(
+                name: "Rezervacija");
+        }
+    }
+}
